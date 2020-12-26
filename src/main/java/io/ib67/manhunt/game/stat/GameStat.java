66e9c9590ea6a40cf352c024043c94661811dc74
Ghost_chu <2908803755@qq.com>
2020-12-27 00:48:27 +0800

    Give some gifts when reach the goals

--- src/main/java/io/ib67/manhunt/game/stat/GameStat.java
+++ src/main/java/io/ib67/manhunt/game/stat/GameStat.java
@@ -1,11 +1,14 @@
 package io.ib67.manhunt.game.stat;
 
+import io.ib67.manhunt.ManHunt;
 import io.ib67.manhunt.game.GamePlayer;
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.advancement.Advancement;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -14,11 +17,32 @@ import java.util.UUID;
 public class GameStat {
     private final Map<UUID, PlayerStat> playerStats = new LinkedHashMap<>();
     @Getter
-    @Setter
     private Phase gamePhase = Phase.GETTING_STARTED;
     @Getter
     @Setter
     private long totalTime;
+
+    public void setGamePhase(Phase gamePhase) {
+        this.gamePhase = gamePhase;
+
+        switch (gamePhase){
+            case GETTING_STARTED:
+                ManHunt.getInstance().getGame().getInGamePlayers().forEach(p->p.getPlayer().getInventory().addItem(new ItemStack(Material.BREAD,5)));
+            case IRON_ARMOR:
+                ManHunt.getInstance().getGame().getInGamePlayers().forEach(p->p.getPlayer().getInventory().addItem(new ItemStack(Material.IRON_INGOT,8)));
+            case IN_NETHER:
+                ManHunt.getInstance().getGame().getInGamePlayers().forEach(p->p.getPlayer().getInventory().addItem(new ItemStack(Material.OBSIDIAN,2)));
+                ManHunt.getInstance().getGame().getInGamePlayers().forEach(p->p.getPlayer().getInventory().addItem(new ItemStack(Material.FLINT,1)));
+            case BLAZE_ROD_GOT:
+                ManHunt.getInstance().getGame().getInGamePlayers().forEach(p->p.getPlayer().getInventory().addItem(new ItemStack(Material.BLAZE_POWDER, 2)));
+            case FIND_STRONGHOLD:
+                ManHunt.getInstance().getGame().getInGamePlayers().forEach(p->p.getPlayer().getInventory().addItem(new ItemStack(Material.ENDER_EYE, 6)));
+            case IN_END:
+                ManHunt.getInstance().getGame().getInGamePlayers().forEach(p->p.getPlayer().getInventory().addItem(new ItemStack(Material.ARROW, 32)));
+            case KILLED_THE_DRAGON:
+                ManHunt.getInstance().getGame().getInGamePlayers().forEach(p->p.getPlayer().getInventory().addItem(new ItemStack(Material.CAKE, 1)));
+        }
+    }
 
     /**
      * @param player
